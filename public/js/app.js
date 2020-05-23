57c8fafd88f97f24f55b3834f74781beab316d0c
Michal Petrik <misopetrik@gmail.com>
2020-05-23 19:55:34 +0200

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = 'Let\'s do this'
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const location = search.value
+
+  messageOne.textContent = 'Getting data...'
+  messageTwo.textContent = ''
+  
+  fetch(`http://localhost:3000/weather?address=${location}`).then(res => {
+    res.json().then(data => {
+      if (data.error) {
+        messageOne.textContent = data.error
+      } else {
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast
+      }
+    })
+  })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=cc27dfb787fafd6785b1b7d58018fbb3&query=${latitude},${longitude}&units=m`;
+  
+  request({ url, json: true}, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather services!');
+    } else if (error) {
+      callback('Unable to find location. Try another search.')
+    } else {
+      callback(undefined, `${body.current.weather_descriptions}. It is currently ${body.location.localtime}. Weather temperature is ${body.current.temperature} degrees Celsius, but it feels like ${body.current.feelslike}.`)
+    }
+  });
+}
+
+module.exports = forecast;
